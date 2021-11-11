3627d686069511214b13629b4a07c122e851c34a
wichaisw <wichai.sw19@gmail.com>
2021-11-11 22:08:57 +0700

    feat: example onOff state

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,11 @@
 import Link from 'next/link';
+import { useState } from 'react';
+
 
 export default function FirstPort() {
+
+  const [onOff, setOnOff] = useState(false);
+
   return (
     <>
       <h1>First Post</h1>
@@ -9,6 +14,8 @@ export default function FirstPort() {
           <a>Back to Home</a>
         </Link>
       </h2>
+      <p>{onOff ? 'On' : 'Off'}</p>
+      <button onClick={() => setOnOff(!onOff)}>Switch</button>
     </>
   )
 }

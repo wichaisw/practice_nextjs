d4c9c0c755eeb7a57d5298c17cb9e8722505bf76
wichaisw <wichai.sw19@gmail.com>
2021-11-11 18:02:57 +0700

    feat: add first page and its routing

---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function FirstPort() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
